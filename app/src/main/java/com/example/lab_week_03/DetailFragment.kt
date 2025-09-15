85ee066bc88e551f4ece72a7f3b7da125fddebb1
Edbert25 <163614909+Edbert25@users.noreply.github.com>
2025-09-15 14:56:13 +0700

    “Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -6,26 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.lab_week_03.R
 
 class DetailFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-    private var coffeeTitle: TextView? = null
-    private var coffeeDesc: TextView? = null
+    private val coffeeTitle: TextView?
+        get() = view?.findViewById(R.id.coffee_title)
+    private val coffeeDesc: TextView?
+        get() = view?.findViewById(R.id.coffee_desc)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
@@ -33,40 +28,24 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        coffeeTitle = view.findViewById(R.id.coffee_title)
-        coffeeDesc = view.findViewById(R.id.coffee_desc)
-        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        val coffeeId = arguments?.getInt(ListFragment.COFFEE_ID, 0) ?: 0
         setCoffeeData(coffeeId)
     }
 
     fun setCoffeeData(id: Int) {
         when (id) {
-            COFFEE_AFFOGATO -> {
+            R.id.affogato -> {
                 coffeeTitle?.text = getString(R.string.affogato_title)
                 coffeeDesc?.text = getString(R.string.affogato_desc)
             }
-            COFFEE_AMERICANO -> {
+            R.id.americano -> {
                 coffeeTitle?.text = getString(R.string.americano_title)
                 coffeeDesc?.text = getString(R.string.americano_desc)
             }
-            COFFEE_LATTE -> {
+            R.id.latte -> {
                 coffeeTitle?.text = getString(R.string.latte_title)
                 coffeeDesc?.text = getString(R.string.latte_desc)
             }
         }
-    }
-
-    companion object {
-        private const val COFFEE_ID = "COFFEE_ID"
-        const val COFFEE_AFFOGATO = 1
-        const val COFFEE_AMERICANO = 2
-        const val COFFEE_LATTE = 3
-
-        fun newInstance(coffeeId: Int) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(COFFEE_ID, coffeeId)
-                }
-            }
     }
 }
--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -1,40 +1,23 @@
 package com.example.lab_week_03
 
-import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.example.lab_week_03.R
+
 
 class ListFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var coffeeListener: CoffeeListener
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is CoffeeListener) {
-            coffeeListener = context
-        } else {
-            throw RuntimeException("Must implement CoffeeListener")
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
@@ -42,37 +25,24 @@ class ListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val coffeeNames = listOf(
-            getString(R.string.affogato_title),
-            getString(R.string.americano_title),
-            getString(R.string.latte_title)
+
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
         )
-        val coffeeIds = listOf(
-            DetailFragment.COFFEE_AFFOGATO,
-            DetailFragment.COFFEE_AMERICANO,
-            DetailFragment.COFFEE_LATTE
-        )
-        val listView = view.findViewById<ListView>(R.id.coffee_list)
-        listView.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, coffeeNames)
-        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            coffeeListener.onSelected(coffeeIds[position])
+
+        coffeeList.forEach { coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener {
+                coffee.findNavController()
+                    .navigate(R.id.coffee_id_action, fragmentBundle)
+            }
         }
     }
 
-    interface CoffeeListener {
-        fun onSelected(coffeeId: Int)
-    }
-
     companion object {
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        const val COFFEE_ID = "COFFEE_ID"
     }
 }
